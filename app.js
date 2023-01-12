61cf4bc8631f03cc4c243476859f820d924bc7a3
BW <bowiewong12@gmail.com>
2023-01-11 23:24:45 -0800

    add fully functioning live chat (private) between 2 users

--- app.js
+++ app.js
@@ -6,6 +6,46 @@ const io = require("socket.io")(8080, {
     }
 })
 
-io.on('connection', (Socket) => {
-    console.log('user Connected')
+let users = []
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId)
+}
+
+const addUser= (userId, socketId) => {
+    !users.some(user=>user.userId === userId) &&
+    users.push ({userId, socketId})
+}
+
+const getUser = (receiverId) => {
+    return users.find(user=>user.userId === receiverId)
+}
+
+io.on('connection', (socket) => {
+    // console.log('user Connected')
+
+    //take ids
+    socket.on('addUser', userId =>{
+        addUser(userId, socket.id)
+        io.emit('getUsers', users)
+    })
+
+    //send msgs
+    socket.on('sendMsg', ({senderId, receiverId, text}) => {
+        // console.log(senderId)
+        const user = getUser(receiverId)
+        console.log(user.socketId)
+        io.to(user.socketId).emit('getMessage', {
+            senderId,
+            text
+        })
+    })
+
+    //disconnect
+    socket.on('disconnect', () => {
+        console.log('user Disconnected')
+        removeUser(socket.id)
+        io.emit('getUsers', users)
+    })
 })
+
